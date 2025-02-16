531a99811355ef80a7f874898f6a8a136e04c81e
amriteshx1 <amriteshx18@gmail.com>
2025-02-16 12:54:50 +0530

    add insert & delete methods for further functionalities

--- bst.js
+++ bst.js
@@ -24,6 +24,53 @@ class Tree{
         }
         return sorted(sortedArr,0,sortedArr.length - 1);
     }
+    
+    //INSERT
+    insert(value){
+        this.root = this._insert(this.root,value);
+    }
+    _insert(node,value){
+        if(node === null)return new Node(value);
+        if(value < node.value){
+            node.left = this._insert(node.left,value);
+        }else if(value > node.value){
+            node.right = this._insert(node.right, value);
+        }
+        return node;
+    }
+
+    //DELETE
+    delete(value){
+        this.root = this._delete(this.root,value);
+    }
+    _delete(node,value){
+        if(node === null)return null;
+        if(value < node.value){
+            node.left = this._delete(node.left,value);
+        }else if(value > node.value){
+            node.right = this._delete(node.right, value);
+        }else {
+            //case 1
+            if(node.left === null && node.right === null)return null;
+            
+            //case 2
+            if(node.left === null)return node.right;
+            if(node.right === null)return node.left;
+
+            //case 3 (inorder successor)
+            let successor = this._minNode(node.right);
+            node.value = successor.value;
+            node.right = this._delete(node.right, successor.value);
+        }
+        return node;
+    }
+    _minNode(node){
+        while (node.left !== null) {
+            node = node.left;
+        }
+        return node;
+    }
+
 
 }
 const a = new Tree([2,3,5,1,4]);
